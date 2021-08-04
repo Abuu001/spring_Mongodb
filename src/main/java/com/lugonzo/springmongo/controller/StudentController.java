b64a990ed5807654190e05e4aca74e04da5ab4fd
Abuu001 <abulugz@gmail.com>
2021-08-04 21:23:59 +0300

    student rest controller

--- src/main/java/com/lugonzo/springmongo/controller/StudentController.java
+++ src/main/java/com/lugonzo/springmongo/controller/StudentController.java
@@ -1,4 +1,23 @@
 package com.lugonzo.springmongo.controller;
 
+import com.lugonzo.springmongo.model.Student;
+import com.lugonzo.springmongo.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping
+    public List<Student> fetchAllStudents(){
+        return studentService.getAllStudents();
+    }
 }
