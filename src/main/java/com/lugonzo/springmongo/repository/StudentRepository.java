7f2d4d068c9b6c4caefd982983c540d91acd5c7d
Abuu001 <abulugz@gmail.com>
2021-08-04 21:25:08 +0300

    mongo repository

--- src/main/java/com/lugonzo/springmongo/repository/StudentRepository.java
+++ src/main/java/com/lugonzo/springmongo/repository/StudentRepository.java
@@ -4,7 +4,12 @@ import com.lugonzo.springmongo.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends MongoRepository<Student,String> {
 
+    //setting custom queries
+
+    Optional<Student> findStudentByEmail(String email);
 }
