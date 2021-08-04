1dacad6630a3ea0b2a9ac2b480161f052dd4bf13
Abuu001 <abulugz@gmail.com>
2021-08-04 22:42:37 +0300

    expense repo

--- src/main/java/com/lugonzo/springmongo/repository/ExpenseRepository.java
+++ src/main/java/com/lugonzo/springmongo/repository/ExpenseRepository.java
@@ -1,2 +1,15 @@
-package com.lugonzo.springmongo.repository;public class ExpenseRepository {
+package com.lugonzo.springmongo.repository;
+
+import com.lugonzo.springmongo.model.Expense;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExpenseRepository extends MongoRepository<Expense,String> {
+
+    @Query("{'name': ?0 }")
+    Optional<Expense> findByName(String name);
 }
