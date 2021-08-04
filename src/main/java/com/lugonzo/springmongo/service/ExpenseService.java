a6f8d82668e286c194128d2c0773c20b17899953
Abuu001 <abulugz@gmail.com>
2021-08-04 22:42:56 +0300

    expense service layer

--- src/main/java/com/lugonzo/springmongo/service/ExpenseService.java
+++ src/main/java/com/lugonzo/springmongo/service/ExpenseService.java
@@ -1,2 +1,46 @@
-package com.lugonzo.springmongo.service;public class ExpenseService {
+package com.lugonzo.springmongo.service;
+
+import com.lugonzo.springmongo.model.Expense;
+import com.lugonzo.springmongo.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public void addExpense(Expense expense) {
+        expenseRepository.insert(expense);
+    }
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public void updateExpense(Expense expense) {
+        Expense savedExpense = expenseRepository.findById(expense.getId())
+                .orElseThrow(()-> new RuntimeException((
+                        String.format("Cannot find expense by ID %s",expense.getId())
+                        )));
+
+        savedExpense.setExpenseAmount(expense.getExpenseAmount());
+        savedExpense.setExpenseName(expense.getExpenseName());
+        savedExpense.setExpenseCategory(expense.getExpenseCategory());
+
+        expenseRepository.save(expense);
+    }
+
+    public Expense getExpenseByName(String expenseName) {
+        return expenseRepository.findByName(expenseName).orElseThrow(()-> new RuntimeException((
+                String.format("Cannot find expense by name -> %s",expenseName)
+                )));
+    }
+
+    public void deleteExpense(String expenseId) {
+        expenseRepository.deleteById(expenseId);
+    }
 }
