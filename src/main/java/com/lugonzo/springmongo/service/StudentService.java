4a5322539a5c73408818f5022acc0c98fddc3714
Abuu001 <abulugz@gmail.com>
2021-08-04 21:24:49 +0300

    mongo service layer

--- src/main/java/com/lugonzo/springmongo/service/StudentService.java
+++ src/main/java/com/lugonzo/springmongo/service/StudentService.java
@@ -1,4 +1,19 @@
 package com.lugonzo.springmongo.service;
 
+import com.lugonzo.springmongo.model.Student;
+import com.lugonzo.springmongo.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
